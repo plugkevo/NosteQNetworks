8c5e8f6119c99ae3668544aed21950ddab9cf226
KEVIN <munenekevin366@gmail.com>
2025-11-07 03:46:12 +0300

    V1.0.5 roll out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.nosteq"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

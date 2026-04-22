079cce2f4381de0b71b1b943ff04b6bd6f9ee92d
KEVIN <munenekevin366@gmail.com>
2026-04-22 14:52:50 +0300

    Quick actions description

--- app/src/main/java/com/kevannTechnologies/nosteqCustomers/DashboardScreen.kt
+++ app/src/main/java/com/kevannTechnologies/nosteqCustomers/DashboardScreen.kt
@@ -246,6 +246,7 @@ fun DashboardScreen(navController: NavController) {
             QuickActionCard(
                 icon = Icons.Filled.List,
                 title = "Packages",
+                description = "Explore plans",
                 modifier = Modifier.weight(1f),
                 onClick = {
                     navController.navigate("packages") {
@@ -260,6 +261,7 @@ fun DashboardScreen(navController: NavController) {
             QuickActionCard(
                 icon = Icons.Filled.AccountBox,
                 title = "Billing",
+                description = "Receipts & Invoices",
                 modifier = Modifier.weight(1f),
                 onClick = {
                     navController.navigate("billing") {
@@ -280,6 +282,7 @@ fun DashboardScreen(navController: NavController) {
             QuickActionCard(
                 icon = Icons.Filled.Router,
                 title = "Router",
+                description = "Manage device",
                 modifier = Modifier.weight(1f),
                 onClick = {
                     navController.navigate("router") {
@@ -294,6 +297,7 @@ fun DashboardScreen(navController: NavController) {
             QuickActionCard(
                 icon = Icons.Filled.Info,
                 title = "Support",
+                description = "Contact us",
                 modifier = Modifier.weight(1f),
                 onClick = {
                     navController.navigate("support") {
@@ -313,6 +317,7 @@ fun DashboardScreen(navController: NavController) {
 fun QuickActionCard(
     icon: ImageVector,
     title: String,
+    description: String,
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
@@ -337,6 +342,11 @@ fun QuickActionCard(
                 text = title,
                 style = MaterialTheme.typography.bodyMedium,
                 fontWeight = FontWeight.Medium
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             )
         }
     }
